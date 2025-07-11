d1863ffe47f98105910e69ba6385d830942c8e8f
Cristian Henrique Calefi Fernandes <111168526+Calefi018@users.noreply.github.com>
2025-07-11 03:13:04 -0300

    Realizado ajustes para dispositivos moveis,
    Ajustes no <iframe> do vídeo do youtube, criação de menu "Hamburguer" para melhor uso em aparelhos celulares etc.

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const hamburguer = document.querySelector(".hamburguer");
+
+const nav = document.querySelector(".nav-cel");
+
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
