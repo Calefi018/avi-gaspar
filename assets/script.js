3e3ac05593723669695c41df7f829848c650a34b
Cristian Henrique Calefi Fernandes <111168526+Calefi018@users.noreply.github.com>
2025-07-11 19:05:22 -0300

    Criação de página de doação
    Página lançada em beta, ainda não funcional.

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,4 @@ const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav-cel");
 
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
